575cfbf71db6f2db9600e53e7d35c020cf147afc
martin-wrede <martin.wrede@targetx.de>
2024-10-11 11:46:57 +0200

    third commit, updated env file

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { Configuration, OpenAIApi } from 'openai'
 
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
 })
 
 const openai = new OpenAIApi(configuration)
